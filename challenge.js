20e1130f57f2912c663c15fe5c62388d562cbdad
AbdifataH-hersi <mohamedabdifatah107@gmail.com>
2024-09-18 08:16:51 +0900

    calculator

--- challenge.js
+++ challenge.js
@@ -1,2 +1,30 @@
 // Good Luck! You got this 💪🏾
 // Write your code here.
+
+
+ 
+
+function calculator(num1, num2, operator) {
+    
+    if (operator === "+") {
+        console.log(num1 + num2);
+    } else if (operator === "-") {
+        console.log(num1 - num2);
+    } else if (operator === "*") {
+        console.log(num1 * num2);
+    } else if (operator === "/") {
+        
+        if (num2 !== 0) {
+            console.log(num1 / num2);
+        } else {
+            console.log("Error: Division by zero is not allowed");
+        }
+    } else {
+        console.log("Invalid operator");
+    }
+}
+
+
+
+    
+ 
